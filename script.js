b923b7214d0f5f396632587a193b10cb325b0985
Nikesh Borkar <99666800+NikeshBorkar@users.noreply.github.com>
2024-07-14 23:45:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+  let sum =0;
+	arr.sort((a,b)=>a-b)
+	while(arr.length>1){
+		let x=arr[0]+arr[1]
+		sum=sum+x
+		arr.shift()
+		arr.shift()
+		arr.unshift(x)
+		arr.sort((a,b)=>a-b)
+	}
+	return sum
 }
 
 module.exports=mincost;
